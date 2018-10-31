3d4a237322ea70da933e29c57d4f4486d9f73320
Alumno <ulsa@iMacD112.local>
2018-10-30 18:32:53 -0700

    holi

---
+++ Pelis/CeldaNoticia.swift
@@ -0,0 +1,14 @@
+//
+//  CeldaNoticia.swift
+//  Pelis
+//
+//  Created by Alumno on 30/10/18.
+//  Copyright © 2018 Alumno. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CeldaNoticia: UITableView {
+    
+}
